f3d4f4c5939f3959f680f8e8602641602bab7904
ㅇㅂㅎ <viewnono1219@gmail.com>
2023-06-10 22:34:37 +0900

    change Spectrum Drawer to draw on self raw image and others...

--- Assets/Scripts/Audio/SpectrumDrawer.cs
+++ Assets/Scripts/Audio/SpectrumDrawer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,52 +7,79 @@ using UnityEngine.UI;
 public class SpectrumDrawer : MonoBehaviour
 {
 	public AudioSource audioSource = null;
-	public RawImage texture = null;
+	private RawImage rawImage = null;
+	private float[] sample;
 
-	public Texture2D DrawAudioSpectrum(Color backgroundColor, Color spectrumColor)
+	[SerializeField] private Color backgroundColor;
+	[SerializeField] private Color spectrumColor;
+
+    private void Awake()
+    {
+        rawImage = GetComponent<RawImage>();
+
+		sample = new float[audioSource.clip.samples * audioSource.clip.channels];
+		audioSource.clip.GetData(sample, 0);
+    }
+
+    private void Start()
+    {
+        DrawAudioSpectrum(.0f, audioSource.clip.length);
+    }
+
+    private float[] GetSpectrum(float startTime, float endTime)
 	{
+		if (startTime > endTime)
+			throw new Exception("startTime is bigger than endTime");
+
+		// Easy to use
 		AudioClip audio = audioSource.clip;
-		int width = (int)texture.rectTransform.rect.width;
-		int height = (int)texture.rectTransform.rect.height;
+        int width = (int)rawImage.rectTransform.rect.width;
+        int height = (int)rawImage.rectTransform.rect.height;
 
-		Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
-		float[] samples = new float[audio.samples * audio.channels];
-		float[] waveform = new float[width];
-		audio.GetData(samples, 0);
+		//Section
+		int sampleSize = audio.samples * audio.channels;
+		int startIndex = (int)(startTime / audio.length * sampleSize); //이상
+		int endIndex = (int)(endTime / audio.length * sampleSize); //초과 
 
-		int s = 0;
-		for(int i = 0; i < samples.Length; i++)
-		{
-			s = (int)(width * ((float)i / samples.Length));
-			if (waveform[s] < Mathf.Abs(samples[i]))
-			{
-				waveform[s] = Mathf.Abs(samples[i]);
-			}
-		}
+		int size = endIndex - startIndex;
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				tex.SetPixel(x, y, backgroundColor);
-			}
-		}
+        float[] waveform = new float[width];
+        int s = 0;
+        for (int i = 0; i < size; i++)
+        {
+            s = (int)(width * ((float)i / size));
 
-		for (int x = 0; x < waveform.Length; x++)
-		{
-			for (int y = 0; y <= waveform[x] * ((float)height * .75f); y++)
-			{
-				tex.SetPixel(x, (height / 2) + y, spectrumColor);
-				tex.SetPixel(x, (height / 2) - y, spectrumColor);
-			}
-		}
-		tex.Apply();
+            if (waveform[s] < Mathf.Abs(sample[i+startIndex])) // Max value
+                waveform[s] = Mathf.Abs(sample[i+startIndex]);
+        }
 
-		return tex;
-	}
+		return waveform;
+    }
 
-	private void Start()
+	public void DrawAudioSpectrum(float startTime, float endTime)
 	{
-		texture.texture = DrawAudioSpectrum(Color.black, Color.white);
+
+        int width = (int)rawImage.rectTransform.rect.width;
+        int height = (int)rawImage.rectTransform.rect.height;
+
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        tex.filterMode = FilterMode.Point;
+
+        float[] waveform = GetSpectrum(startTime, endTime);
+
+		// Fill Background
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < height; y++)
+                tex.SetPixel(x, y, backgroundColor);
+
+		// Draw Spectrum
+		for (int x = 0; x < waveform.Length; x++)
+			for (int y = 0; y <= waveform[x] * height; y++)
+                tex.SetPixel(x, y, spectrumColor);
+
+        tex.Apply();
+
+
+        rawImage.texture = tex;
 	}
 }
